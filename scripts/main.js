8a0d55fad2a8aa032de20539e6e05656f3290a04
Guilherme Rezende Silva <rezendeguilherme733@gmail.com>
2023-04-13 16:24:20 -0300

    MathTech upload

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+function Soma () {
+    let soma1 = Number(document.getElementById("soma1").value)
+    let soma2 = Number(document.getElementById("soma2").value)
+    let soma =  soma1 + soma2
+    let somaResultado = document.getElementById("somaResult").innerText = soma
+
+}
+
+function Sub (){
+    let sub1 = Number(document.getElementById("sub1").value)
+    let sub2 = Number(document.getElementById("sub2").value)
+    let sub =  sub1 - sub2
+    let subResultado = document.getElementById("subResult").innerText = sub
+}
+function Div (){
+    let div1 = Number(document.getElementById("div1").value)
+    let div2 = Number(document.getElementById("div2").value)
+    let div =  div1 / div2
+    let divResultado = document.getElementById("divResult").innerText = div
+    
+}
+function Multi(){
+    let multi1 = Number(document.getElementById("multi1").value)
+    let multi2 = Number(document.getElementById("multi2").value)
+    let multi =  multi1 * multi2
+    let multiResultado = document.getElementById("multiResult").innerText = multi
+}
